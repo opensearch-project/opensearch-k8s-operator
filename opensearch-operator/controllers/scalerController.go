3df9c37572d7b5dd14040692b9cb9abcba720db1
Sebastian Woehrl <sebastian.woehrl@maibornwolff.de>
2022-02-15 13:12:02 +0100

    Comment scaler logic back in

--- opensearch-operator/controllers/scalerController.go
+++ opensearch-operator/controllers/scalerController.go
@@ -2,11 +2,14 @@ package controllers
 
 import (
 	"context"
+	"errors"
+	"fmt"
 
 	"github.com/go-logr/logr"
 	appsv1 "k8s.io/api/apps/v1"
 	"k8s.io/client-go/tools/record"
 	opsterv1 "opensearch.opster.io/api/v1"
+	"opensearch.opster.io/pkg/helpers"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
@@ -44,72 +47,65 @@ func (r *ScalerReconciler) reconcileNodePool(nodePool *opsterv1.NodePool, contro
 
 	if *nodePoolSTS.Spec.Replicas == nodePool.Replicas {
 		return nil, nil
-	} else {
-		nodePoolSTS.Spec.Replicas = &nodePool.Replicas
-		err := r.Update(context.TODO(), &nodePoolSTS)
-		return nil, err
-		// TODO: Implement actual scaling logic
-		// var found bool
-		// name := fmt.Sprintf("Scaler-%s", nodePool.Component)
-		// var componentStatus opsterv1.ComponentStatus
-		// comp := r.Instance.Status.ComponentsStatus
-		// for i := 0; i < len(comp); i++ {
-		// 	if comp[i].Component == "Scaler" {
-		// 		if comp[i].Description == group {
-		// 			found = true
-		// 			if comp[i].Status == "Running" {
-		// 				// --- Now check if scaling is done logic ----
-		// 				done := true
-		// 				if !done {
-		// 					r.Recorder.Event(r.Instance, "Normal", "one scale is already in progress on that group ", "one scale is already in progress on that group")
-		// 					return ctrl.Result{}, nil
-		// 				} else {
-		// 					// if scale logic is done - remove componentStatus
-		// 					componentStatus = opsterv1.ComponentStatus{
-		// 						Component:   "Scaler",
-		// 						Status:      "Running",
-		// 						Description: group,
-		// 					}
-		// 					newStatus := helpers.RemoveIt(componentStatus, comp)
-		// 					r.Instance.Status.ComponentsStatus = newStatus
-		// 					if err := r.Status().Update(ctx, r.Instance); err != nil {
-		// 						err = errors.New("cannot update instance status")
-		// 						return ctrl.Result{}, err
-		// 					}
-
-		// 					//r.Recorder.Event(r.Instance, "Normal", "done scaling", "done scaling")
-		// 					nodePoolSTS.Spec.Replicas = &nodePool.Replicas
-		// 					if err := r.Update(ctx, &nodePoolSTS); err != nil {
-		// 						err = errors.New("cannot update instance status")
-
-		// 						return ctrl.Result{}, err
-		// 					}
-		// 					return ctrl.Result{}, nil
-		// 				}
-		// 			} else if comp[i].Status == "Failed" {
-		// 				r.Recorder.Event(r.Instance, "Normal", "something want worng with scaling operation", "something went worng)")
-		// 				return ctrl.Result{}, nil
-		// 			}
-		// 		}
-		// 	}
-		// }
-		// // if not found componentStatus and replicas not equal
-		// if !found {
-		// 	// starting new componentStatus
-		// 	componentStatus = opsterv1.ComponentStatus{
-		// 		Component:   "Scaler",
-		// 		Status:      "Running",
-		// 		Description: group,
-		// 	}
-		// 	//r.Recorder.Event(r.Instance, "Normal", "add new status event about scale ", "add new status event about scale ")
-
-		// 	r.Instance.Status.ComponentsStatus = append(r.Instance.Status.ComponentsStatus, componentStatus)
-		// 	if err := r.Status().Update(ctx, r.Instance); err != nil {
-		// 		err = errors.New("cannot update instance status")
-		// 		return ctrl.Result{}, err
-		// 	}
-		// 	// -----  Now start scaling logic ------
-
-		// }
 	}
+	var found bool
+	name := fmt.Sprintf("Scaler-%s", nodePool.Component)
+	for _, existingStatus := range r.Instance.Status.ComponentsStatus {
+		if existingStatus.Component == name {
+			found = true
+			if existingStatus.Status == "Running" {
+				// --- Now check if scaling is done logic ----
+				done := true
+				if !done {
+					r.Recorder.Event(r.Instance, "Normal", "one scale is already in progress on that group ", "one scale is already in progress on that group")
+					return nil, nil
+				} else {
+					// if scale logic is done - remove componentStatus
+					componentStatus := opsterv1.ComponentStatus{
+						Component:   name,
+						Status:      "Running",
+						Description: "",
+					}
+					newStatus := helpers.RemoveIt(componentStatus, r.Instance.Status.ComponentsStatus)
+					r.Instance.Status.ComponentsStatus = newStatus
+					if err := r.Status().Update(context.TODO(), r.Instance); err != nil {
+						err = errors.New("cannot update instance status")
+						return nil, err
+					}
+
+					//r.Recorder.Event(r.Instance, "Normal", "done scaling", "done scaling")
+					nodePoolSTS.Spec.Replicas = &nodePool.Replicas
+					if err := r.Update(context.TODO(), &nodePoolSTS); err != nil {
+						err = errors.New("cannot update instance status")
+
+						return nil, err
+					}
+					return nil, nil
+				}
+			} else if existingStatus.Status == "Failed" {
+				r.Recorder.Event(r.Instance, "Normal", "something want worng with scaling operation", "something went worng)")
+				return nil, nil
+			}
+		}
+	}
+	// if not found componentStatus and replicas not equal
+	if !found {
+		// starting new componentStatus
+		componentStatus := opsterv1.ComponentStatus{
+			Component:   name,
+			Status:      "Running",
+			Description: "",
+		}
+		//r.Recorder.Event(r.Instance, "Normal", "add new status event about scale ", "add new status event about scale ")
+
+		r.Instance.Status.ComponentsStatus = append(r.Instance.Status.ComponentsStatus, componentStatus)
+		if err := r.Status().Update(context.TODO(), r.Instance); err != nil {
+			err = errors.New("cannot update instance status")
+			return nil, err
+		}
+		// -----  Now start scaling logic ------
+
+	}
+
+	return nil, nil
 }
