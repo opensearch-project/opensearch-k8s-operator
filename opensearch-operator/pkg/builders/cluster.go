bcbb7e2f3cbb65d1090b7218696b29f9b0299f15
idanl21 <idan@opster.com>
2022-07-10 12:18:46 +0300

    remove role restrictions.

--- opensearch-operator/pkg/builders/cluster.go
+++ opensearch-operator/pkg/builders/cluster.go
@@ -47,15 +47,15 @@ func NewSTSForNodePool(
 	availableRoles := []string{
 		"master",
 		"data",
-		//"data_content",
-		//"data_hot",
-		//"data_warm",:
-		//"data_cold",
-		//"data_frozen",
+		"data_content",
+		"data_hot",
+		"data_warm",
+		"data_cold",
+		"data_frozen",
 		"ingest",
-		//"ml",
-		//"remote_cluster_client",
-		//"transform",
+		"ml",
+		"remote_cluster_client",
+		"transform",
 	}
 	var selectedRoles []string
 	for _, role := range node.Roles {
