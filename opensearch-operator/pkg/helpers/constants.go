d006e5171b6852083523f5aefb79b7d2f83c812f
idanl21 <idan@opster.com>
2023-04-13 18:54:31 +0300

    fix conflicts

--- opensearch-operator/pkg/helpers/constants.go
+++ opensearch-operator/pkg/helpers/constants.go
@@ -3,12 +3,15 @@ package helpers
 import "os"
 
 const (
-	DashboardConfigName       = "opensearch_dashboards.yml"
-	DashboardChecksumName     = "checksum/dashboards.yml"
+	DashboardConfigName   = "opensearch_dashboards.yml"
+	DashboardChecksumName = "checksum/dashboards.yml"
+	ClusterLabels         = "opster.io/opensearch-cluster"
+	//NodePoolLabels                = "opster.io/opensearch-nodepool"
 	OsUserNameAnnotation      = "opensearchuser/name"
 	OsUserNamespaceAnnotation = "opensearchuser/namespace"
 	DnsBaseEnvVariable        = "DNS_BASE"
-	ClusterLabels             = "opster.io/opensearch-cluster"
+	//ParallelRecoveryEnabled      = "PARALLEL_RECOVERY_ENABLED"
+	//SkipInitContainerEnvVariable = "SKIP_INIT_CONTAINER"
 )
 
 func ClusterDnsBase() string {
