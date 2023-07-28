0b8067a4cb16f17ccf451757efef02e64c10ab49
Isha Girdhar <isha.girdhar@oracle.com>
2023-07-28 18:51:28 +0530

    Revert go.work Signed-off-by: Isha Girdhar <isha.girdhar@oracle.com>

--- opensearch-operator/main.go
+++ opensearch-operator/main.go
@@ -58,7 +58,7 @@ func main() {
 	var watchNamespace string
 	var logLevel string
 	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8082", "The address the probe endpoint binds to.")
+	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
 			"Enabling this will ensure there is only one active controller manager.")
