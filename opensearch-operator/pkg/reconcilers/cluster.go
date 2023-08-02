dce4cf95a62df7c09a28b82077482bed1473b4bb
idanl21 <idan@opster.com>
2023-08-02 16:00:19 +0300

    check functional tests

--- opensearch-operator/pkg/reconcilers/cluster.go
+++ opensearch-operator/pkg/reconcilers/cluster.go
@@ -317,22 +317,21 @@ func (r *ClusterReconciler) reconcileNodeStatefulSet(nodePool opsterv1.NodePool,
 		if err != nil {
 			return result, err
 		}
+	} else {
+		// if pdb is not enabled and pdb resource exist, deleting it
+		nsn := types.NamespacedName{
+			Namespace: r.instance.Namespace,
+			Name:      r.instance.Name + "-" + nodePool.Component + "-pdb",
+		}
+		if err = r.Get(r.ctx, nsn, &pdb); err == nil {
+			opts := client.DeleteOptions{}
+			r.logger.Info("Deleting pdb" + pdb.Name)
+			if err = r.Delete(r.ctx, sts, &opts); err != nil {
+				r.logger.Info("Tried to delete" + pdb.Name + "but got an  error")
+			}
+
+		}
 	}
-	//} else {
-	//	// if pdb is not enabled and pdb resource exist, deleting it
-	//	nsn := types.NamespacedName{
-	//		Namespace: r.instance.Namespace,
-	//		Name:      r.instance.Name + "-" + nodePool.Component + "-pdb",
-	//	}
-	//	if err := r.Get(r.ctx, nsn, &pdb); err == nil {
-	//		opts := client.DeleteOptions{}
-	//		r.logger.Info("Deleting pdb" + pdb.Name)
-	//		if err = r.Delete(r.ctx, sts, &opts); err != nil {
-	//			r.logger.Info("Tried to delete" + pdb.Name + "but got an  error")
-	//		}
-	//
-	//	}
-	//}
 	// Handle PVC resizing
 
 	//Default is PVC, or explicit check for PersistenceSource as PVC
