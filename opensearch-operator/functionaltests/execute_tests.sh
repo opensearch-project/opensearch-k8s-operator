9634e3e0e8fc79cb39817556a740729b2488930a
Sebastian Woehrl <sebastian.woehrl@maibornwolff.de>
2023-08-09 08:58:21 +0200

    Add missing rbac rule for PDB
    Signed-off-by: Sebastian Woehrl <sebastian.woehrl@maibornwolff.de>

--- opensearch-operator/functionaltests/execute_tests.sh
+++ opensearch-operator/functionaltests/execute_tests.sh
@@ -2,7 +2,7 @@
 CLUSTER_NAME=opensearch-operator-tests
 
 ## Setup k3d cluster and prepare kubeconfig
-k3d cluster create $CLUSTER_NAME --agents 2 --kubeconfig-switch-context=false --kubeconfig-update-default=false -p "30000-30005:30000-30005@agent:0"
+k3d cluster create $CLUSTER_NAME --agents 2 --kubeconfig-switch-context=false --kubeconfig-update-default=false -p "30000-30005:30000-30005@agent:0" --image=rancher/k3s:v1.22.17-k3s1
 k3d kubeconfig get $CLUSTER_NAME > kubeconfig
 export KUBECONFIG=$(pwd)/kubeconfig
 
@@ -21,6 +21,6 @@ cd functionaltests
 
 ## Run tests
 go test ./operatortests -timeout 30m
-go test ./helmtests -timeout 15m
+go test ./helmtests -timeout 20m
 ## Delete k3d cluster
 k3d cluster delete $CLUSTER_NAME
