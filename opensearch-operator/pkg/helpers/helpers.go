60f300273b38e90524b680081b71816e45b662b1
idanl21 <idan@opster.com>
2023-08-21 16:27:30 +0300

    fix lint

--- opensearch-operator/pkg/helpers/helpers.go
+++ opensearch-operator/pkg/helpers/helpers.go
@@ -8,9 +8,6 @@ import (
 	"sort"
 	"time"
 
-	batchv1 "k8s.io/api/batch/v1"
-	"k8s.io/apimachinery/pkg/types"
-
 	"github.com/hashicorp/go-version"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
