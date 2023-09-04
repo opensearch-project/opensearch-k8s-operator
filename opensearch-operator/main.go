97968ab680fa3079a10ee68de9de089353e05a7f
idanl21 <idan@opster.com>
2023-09-04 12:51:16 +0300

    fix main

--- opensearch-operator/main.go
+++ opensearch-operator/main.go
@@ -145,15 +145,14 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "OpensearchUserRoleBinding")
 		os.Exit(1)
 	}
-	//if err = (&controllers.OpensearchActionGroupReconciler{
-	//	Client:   mgr.GetClient(),
-	//	Scheme:   mgr.GetScheme(),
-	//	Recorder: mgr.GetEventRecorderFor("actiongroup-controller"),
-	//}).SetupWithManager(mgr); err != nil {
-	//	setupLog.Error(err, "unable to create controller", "controller", "OpensearchActionGroup")
-	//	os.Exit(1)
-	//}
-	//+kubebuilder:scaffold:builder
+	if err = (&controllers.OpensearchActionGroupReconciler{
+		Client:   mgr.GetClient(),
+		Scheme:   mgr.GetScheme(),
+		Recorder: mgr.GetEventRecorderFor("actiongroup-controller"),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "OpensearchActionGroup")
+		os.Exit(1)
+	}
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
