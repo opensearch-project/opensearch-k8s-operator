e0374040b4a99876cdec0e7881116dfaa8616d03
Yevhenii Tiutiunnyk <evheniytyutyunnik@gmail.com>
2024-02-25 13:23:07 +0200

    Use cmp instead of reflect, fix template reconciler
    Signed-off-by: Yevhenii Tiutiunnyk <evheniytyutyunnik@gmail.com>

--- opensearch-operator/pkg/helpers/helpers.go
+++ opensearch-operator/pkg/helpers/helpers.go
@@ -1,10 +1,13 @@
 package helpers
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"reflect"
 	"sort"
+	"strings"
 	"time"
 
 	policyv1 "k8s.io/api/policy/v1"
@@ -147,6 +150,26 @@ func SortedKeys(input map[string]string) []string {
 	}
 	sort.Strings(keys)
 	return keys
+}
+
+// SortedJsonKeys helps to sort and format JSON object keys
+// E.g. if API returns unsorted JSON object like this: {"resp": {"b": "2", "a": "1"}}
+// this function could sort it and return {"resp": {"a": "1", "b": "2"}}
+// This is useful for comparing Opensearch CRD objects and API responses
+func SortedJsonKeys(obj *apiextensionsv1.JSON) (*apiextensionsv1.JSON, error) {
+	m := make(map[string]interface{})
+	if err := json.Unmarshal(obj.Raw, &m); err != nil {
+		return nil, err
+	}
+	rawBytes, err := json.MarshalIndent(m, "", "")
+	if err != nil {
+		return nil, err
+	}
+	jsonString := strings.ReplaceAll(string(rawBytes), "\n", "")
+	jsonString = strings.ReplaceAll(jsonString, "\",\"", "\", \"")
+	obj = &apiextensionsv1.JSON{Raw: []byte(jsonString)}
+	fmt.Println(jsonString)
+	return obj, err
 }
 
 func ResolveClusterManagerRole(ver string) string {
