ea46394e62ea63f7d997f4ab88d5eae5badf1e3b
Nilushan Costa <nilushan@wso2.com>
2024-05-13 17:20:40 +0530

    Add retry for opensearch client creation in ISM Policy reconciler (#805)
    ### Description
    Add retry for opensearch client creation in ISM policy reconciler to fix
    panic
    Minor change - Remove extra whitespace in developing.md file
    
    ### Issues Resolved
    Resolves
    https://github.com/opensearch-project/opensearch-k8s-operator/issues/801
    
    ### Check List
    - [x] Commits are signed per the DCO using --signoff 
    - [ ] Unittest added for the new/changed functionality and all unit
    tests are successful
    - [ ] Customer-visible features documented
    - [x] No linter warnings (`make lint`)
    
    If CRDs are changed:
    - [ ] CRD YAMLs updated (`make manifests`) and also copied into the helm
    chart
    - [ ] Changes to CRDs documented
    
    Please refer to the [PR
    guidelines](https://github.com/opensearch-project/opensearch-k8s-operator/blob/main/docs/developing.md#submitting-a-pr)
    before submitting this pull request.
    
    By submitting this pull request, I confirm that my contribution is made
    under the terms of the Apache 2.0 license.
    For more information on following Developer Certificate of Origin and
    signing off your commits, please check
    [here](https://github.com/opensearch-project/OpenSearch/blob/main/CONTRIBUTING.md#developer-certificate-of-origin).
    
    ---------
    
    Signed-off-by: Nilushan Costa <nilushan@wso2.com>

--- opensearch-operator/pkg/reconcilers/ismpolicy.go
+++ opensearch-operator/pkg/reconcilers/ismpolicy.go
@@ -146,6 +146,12 @@ func (r *IsmPolicyReconciler) Reconcile() (retResult ctrl.Result, retErr error) 
 	if err != nil {
 		reason := "error creating opensearch client"
 		r.recorder.Event(r.instance, "Warning", opensearchError, reason)
+		retResult = ctrl.Result{
+			Requeue:      true,
+			RequeueAfter: 30 * time.Second,
+		}
+		retErr = err
+		return
 	}
 
 	// If PolicyID not provided explicitly, use metadata.name by default
