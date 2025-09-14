1d508d02728be627d4512729d2632cd1fa79b555
Itamar Syn-Hershko <itamar@bigdataboutique.com>
2025-09-14 19:46:58 +0300

    Remove kube-rbac-proxy in favor of WithAuthenticationAndAuthorization
    Signed-off-by: Itamar Syn-Hershko <itamar@bigdataboutique.com>

--- opensearch-operator/main.go
+++ opensearch-operator/main.go
@@ -24,6 +24,7 @@ import (
 
 	"github.com/Opster/opensearch-k8s-operator/opensearch-operator/pkg/helpers"
 	"sigs.k8s.io/controller-runtime/pkg/cache"
+	"sigs.k8s.io/controller-runtime/pkg/metrics/filters"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 
@@ -65,7 +66,7 @@ func main() {
 	var probeAddr string
 	var watchNamespace string
 	var logLevel string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
+	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8443", "The address the metric endpoint binds to.")
 	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
 	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
 		"Enable leader election for controller manager. "+
@@ -121,7 +122,9 @@ func main() {
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme: scheme,
 		Metrics: metricsserver.Options{
-			BindAddress: metricsAddr,
+			BindAddress:    metricsAddr,
+			SecureServing:  true,
+			FilterProvider: filters.WithAuthenticationAndAuthorization,
 		},
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
