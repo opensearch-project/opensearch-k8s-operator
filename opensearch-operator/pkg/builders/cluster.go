50d32a49319deb8ef0cb302aeec4ad26c3c88eac
André Bauer <monotek23@gmail.com>
2025-09-24 10:46:37 +0200

    add opster label to securityconfig-update job and pod
    Signed-off-by: André Bauer <monotek23@gmail.com>

--- opensearch-operator/pkg/builders/cluster.go
+++ opensearch-operator/pkg/builders/cluster.go
@@ -3,8 +3,9 @@ package builders
 import (
 	"context"
 	"fmt"
-	"k8s.io/utils/ptr"
 	"strings"
+
+	"k8s.io/utils/ptr"
 
 	monitoring "github.com/prometheus-operator/prometheus-operator/pkg/apis/monitoring/v1"
 
@@ -1139,11 +1140,23 @@ func NewSecurityconfigUpdateJob(
 	podSecurityContext := instance.Spec.General.PodSecurityContext
 	resources := instance.Spec.Security.GetConfig().GetUpdateJob().Resources
 	return batchv1.Job{
-		ObjectMeta: metav1.ObjectMeta{Name: jobName, Namespace: namespace, Annotations: annotations},
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        jobName,
+			Namespace:   namespace,
+			Annotations: annotations,
+			Labels: map[string]string{
+				helpers.JobLabel: instance.Name,
+			},
+		},
 		Spec: batchv1.JobSpec{
 			BackoffLimit: &backoffLimit,
 			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{Name: jobName},
+				ObjectMeta: metav1.ObjectMeta{
+					Name: jobName,
+					Labels: map[string]string{
+						helpers.JobLabel: instance.Name,
+					},
+				},
 				Spec: corev1.PodSpec{
 					TerminationGracePeriodSeconds: &terminationGracePeriodSeconds,
 					Containers: []corev1.Container{{
