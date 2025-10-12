562d2f9d8c09bddeb23b52c60537c5f6b716d84c
josedev-union <josebarato321@gmail.com>
2025-10-12 08:09:56 +0200

    fix: correct namespace restriction for reconcile watch
    Signed-off-by: josedev-union <josebarato321@gmail.com>

--- opensearch-operator/main.go
+++ opensearch-operator/main.go
@@ -73,12 +73,6 @@ func main() {
 		"The namespace that controller manager is restricted to watch. If not set, default is to watch all namespaces.")
 	flag.StringVar(&logLevel, "loglevel", "info", "The log level to use for the operator logs. Possible values: debug,info,warn,error")
 
-	var cacheOpts cache.Options
-	if watchNamespace != "" {
-		cacheOpts.DefaultNamespaces = map[string]cache.Config{
-			watchNamespace: {},
-		}
-	}
 	opts := zap.Options{
 		Development: false,
 		TimeEncoder: zapcore.ISO8601TimeEncoder,
@@ -93,6 +87,13 @@ func main() {
 
 	opts.BindFlags(flag.CommandLine)
 	flag.Parse()
+
+	var cacheOpts cache.Options
+	if watchNamespace != "" {
+		cacheOpts.DefaultNamespaces = map[string]cache.Config{
+			watchNamespace: {},
+		}
+	}
 
 	level, err := zapcore.ParseLevel(logLevel)
 	if err != nil {
