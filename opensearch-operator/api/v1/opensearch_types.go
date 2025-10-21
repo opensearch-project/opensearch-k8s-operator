d8e6dcd62e0d4f531c80fa43327a59b4e344d44f
josedev-union <josebarato321@gmail.com>
2025-10-21 16:55:00 +0200

    fix kube validation
    Signed-off-by: josedev-union <josebarato321@gmail.com>

--- opensearch-operator/api/v1/opensearch_types.go
+++ opensearch-operator/api/v1/opensearch_types.go
@@ -129,7 +129,9 @@ type NodePool struct {
 	// +kubebuilder:pruning:PreserveUnknownFields
 	// +kubebuilder:validation:Schemaless
 	SidecarContainers []corev1.Container `json:"sidecarContainers,omitempty"`
-	InitContainers    []corev1.Container `json:"initContainers,omitempty"`
+	// +kubebuilder:pruning:PreserveUnknownFields
+	// +kubebuilder:validation:Schemaless
+	InitContainers []corev1.Container `json:"initContainers,omitempty"`
 }
 
 // PersistencConfig defines options for data persistence
@@ -176,11 +178,13 @@ type BootstrapConfig struct {
 	Affinity     *corev1.Affinity            `json:"affinity,omitempty"`
 	Jvm          string                      `json:"jvm,omitempty"`
 	// Extra items to add to the opensearch.yml, defaults to General.AdditionalConfig
-	AdditionalConfig map[string]string  `json:"additionalConfig,omitempty"`
-	Annotations      map[string]string  `json:"annotations,omitempty"`
-	PluginsList      []string           `json:"pluginsList,omitempty"`
-	Keystore         []KeystoreValue    `json:"keystore,omitempty"`
-	InitContainers   []corev1.Container `json:"initContainers,omitempty"`
+	AdditionalConfig map[string]string `json:"additionalConfig,omitempty"`
+	Annotations      map[string]string `json:"annotations,omitempty"`
+	PluginsList      []string          `json:"pluginsList,omitempty"`
+	Keystore         []KeystoreValue   `json:"keystore,omitempty"`
+	// +kubebuilder:pruning:PreserveUnknownFields
+	// +kubebuilder:validation:Schemaless
+	InitContainers []corev1.Container `json:"initContainers,omitempty"`
 }
 
 type DashboardsServiceSpec struct {
