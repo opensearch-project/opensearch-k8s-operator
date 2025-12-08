69b6df250b65b303c6937225168d551188862653
Alessandro Fulgini <fuljo97@gmail.com>
2025-12-08 16:40:41 +0100

    fix: resolve node version mismatch deadlock when upgrading
    Fixes #1153
    
    Signed-off-by: Alessandro Fulgini <fuljo97@gmail.com>

--- opensearch-operator/opensearch-gateway/services/errors.go
+++ opensearch-operator/opensearch-gateway/services/errors.go
@@ -6,10 +6,15 @@ import (
 )
 
 var (
-	ErrClusterHealthOperation   = errors.New("cluster health failed")
-	ErrClusterSettingsOperation = errors.New("cluster settings failed")
-	ErrCatIndicesOperation      = errors.New("cat indices failed")
+	ErrClusterAllocationExplainOperation = errors.New("cluster allocation explain failed")
+	ErrClusterHealthOperation            = errors.New("cluster health failed")
+	ErrClusterSettingsOperation          = errors.New("cluster settings failed")
+	ErrCatIndicesOperation               = errors.New("cat indices failed")
 )
+
+func ErrClusterAllocationExplainGetFailed(resp string) error {
+	return fmt.Errorf("get error %w: %s", ErrClusterAllocationExplainOperation, resp)
+}
 
 func ErrClusterHealthGetFailed(resp string) error {
 	return fmt.Errorf("get error %w: %s", ErrClusterHealthOperation, resp)
