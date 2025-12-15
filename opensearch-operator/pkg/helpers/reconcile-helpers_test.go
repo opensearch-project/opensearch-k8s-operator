bc8c208a5875d196d1c451dc33e420d7c1dcb3d3
josedev-union <josebarato321@gmail.com>
2025-12-15 13:16:54 +0100

    fix: handle pre-release versions in version constraint checker
    fix https://github.com/opensearch-project/opensearch-k8s-operator/issues/1013
    
    Signed-off-by: josedev-union <josebarato321@gmail.com>

--- opensearch-operator/pkg/helpers/reconcile-helpers_test.go
+++ opensearch-operator/pkg/helpers/reconcile-helpers_test.go
@@ -27,4 +27,7 @@ var _ = DescribeTable("versionCheck reconciler",
 	Entry("When no http port is specified and version 2.0 is used", "2.0", int32(0), int32(9200), int32(9200), "/usr/share/opensearch/config/opensearch-security"),
 	Entry("When an http port is specified and version 1.3.0 is used", "1.3.0", int32(6000), int32(6000), int32(9300), "/usr/share/opensearch/plugins/opensearch-security/securityconfig"),
 	Entry("When an http port is specified and version 2.0 is used", "2.0", int32(6000), int32(6000), int32(6000), "/usr/share/opensearch/config/opensearch-security"),
+	Entry("When no http port is specified and prerelease version 3.0.0-testing is used", "3.0.0-testing", int32(0), int32(9200), int32(9200), "/usr/share/opensearch/config/opensearch-security"),
+	Entry("When no http port is specified and prerelease version 2.0.0-testing is used", "2.0.0-testing", int32(0), int32(9200), int32(9200), "/usr/share/opensearch/config/opensearch-security"),
+	Entry("When no http port is specified and prerelease version 1.9.0-testing is used", "1.9.0-testing", int32(0), int32(9200), int32(9300), "/usr/share/opensearch/plugins/opensearch-security/securityconfig"),
 )
