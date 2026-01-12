41283efc00c91ab75635edfd1beb8f1d6f94d4ef
josedev-union <josebarato321@gmail.com>
2026-01-12 16:50:45 +0100

    feat: add operator version upgrade test
    Signed-off-by: josedev-union <josebarato321@gmail.com>

--- opensearch-operator/functionaltests/operatortests/main_test.go
+++ opensearch-operator/functionaltests/operatortests/main_test.go
@@ -4,24 +4,31 @@ import (
 	"testing"
 
 	opsterv1 "github.com/opensearch-project/opensearch-k8s-operator/opensearch-operator/api/v1"
+	"github.com/go-logr/logr"
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
 	"k8s.io/client-go/tools/clientcmd"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	clog "sigs.k8s.io/controller-runtime/pkg/log"
 )
 
 var k8sClient client.Client
 
 func TestAPIs(t *testing.T) {
+	// Set up controller-runtime logger to avoid warnings
+	clog.SetLogger(logr.Discard())
+
 	config, err := clientcmd.BuildConfigFromFlags("", "../kubeconfig")
 	if err != nil {
 		panic(err.Error())
 	}
 	scheme := runtime.NewScheme()
 	utilruntime.Must(clientgoscheme.AddToScheme(scheme))
+	utilruntime.Must(apiextensionsv1.AddToScheme(scheme))
 	utilruntime.Must(opsterv1.AddToScheme(scheme))
 	k8sClient, err = client.New(config, client.Options{
 		Scheme: scheme,
--- opensearch-operator/functionaltests/operatortests/suite_test.go
+++ opensearch-operator/functionaltests/operatortests/suite_test.go
@@ -1,19 +1,38 @@
 package operatortests
 
 import (
+	"context"
+
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
+	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 // Global setup/teardown for data integrity scenarios.
 // This ensures the test-cluster cluster exists before any
 // focused scenario (e.g. "Scale up scenario") is executed.
+// For operator upgrade tests, this will be skipped if the operator isn't installed yet.
 
 var _ = BeforeSuite(func() {
+	// Check if the OpenSearchCluster CRD exists (operator is installed)
+	// If not, skip creating the cluster (for operator upgrade test)
+	crd := &apiextensionsv1.CustomResourceDefinition{}
+	err := k8sClient.Get(context.Background(), client.ObjectKey{Name: "opensearchclusters.opensearch.opster.io"}, crd)
+	if errors.IsNotFound(err) {
+		By("OpenSearchCluster CRD not found - skipping test-cluster creation (operator upgrade test will install operator first)")
+		return
+	}
+	if err != nil {
+		// Some other error occurred, but we'll try to continue anyway
+		By("Warning: Could not check if OpenSearchCluster CRD exists, attempting to create cluster anyway")
+	}
+
 	By("Creating OpenSearchCluster 'test-cluster'")
 	// Create the OpenSearchCluster used by all data integrity tests.
 	// The manifest lives under test-cluster.yaml.
-	err := CreateKubernetesObjects("test-cluster")
+	err = CreateKubernetesObjects("test-cluster")
 	Expect(err).NotTo(HaveOccurred())
 })
 
